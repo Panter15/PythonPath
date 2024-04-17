b8c0e93f6bd7195f984e9137e6a16b2d098020ae
l-portilla <l.portilla@dxc.com>
2024-04-16 19:29:26 -0600

    My first program in python

---
+++ FirstProgram/FirstAlgorythm.py
@@ -0,0 +1,6 @@
+base = float(input("Ingresa la base del triangulo: "))
+altura = float(input("Ingresa la altura del triangulo: "))
+
+area = (base*altura)/2
+
+print("El area del triangulo es: ", area)
